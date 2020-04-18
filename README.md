eb5662fbb650ebac921359a35ce0e6abe8dd52c4
bartequ <bartlomiej.szabat@gmail.com>
2020-04-18 23:00:45 +0200

    Node time counting fix

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Backend in Spring and Node for purpose of Master Thesis
 ## Purpose of Master Thesis is to compare performance between Spring and Node in bunch of scenarios
 
+### Setup mock server
+- `docker run -d --rm -p 1080:1080 mockserver/mockserver`
+- `curl -v -X PUT "http://localhost:1080/mockserver/expectation" --data "@[path_to_mock_server_expectation_file]"`
+<br>In this exampleexpectation file it's just @backup.json
+- `http://localhost:1080/api/photos/[number_of_objects]`
+
 ### Spring endpoints used for tests
 #### Time consuming operations
 - `GET /api/prime-numbers/[number_to_count]`
@@ -22,8 +28,3 @@
 - `POST /api/database-operations`
 - `PUT /api/database-operations/id`
 - `DELETE /api/database-operations/id`
-
-### Setup mock server
-`docker run -d --rm -p 1080:1080 mockserver/mockserver`<br>
-`curl -v -X PUT "http://localhost:1080/mockserver/expectation" --data "@backup.json"`
-`http://localhost:1080/api/photos/5000`
--- mgr-backend-node/api/fetchData.js
+++ mgr-backend-node/api/fetchData.js
@@ -1,17 +1,9 @@
 const axios = require('axios');
 const url = 'http://localhost:1080/api/photos/5000';
 
-function PhotoDto(id, albumId, title, url, thumbnailUrl) {
-    this.id = id;
-    this.albumId = albumId;
-    this.title = title;
-    this.url = url;
-    this.thumbnailUrl = thumbnailUrl;
-}
-
 function fetchTestPhotosToObj(url) {
+    let t0 = process.hrtime();
     axios.get(url).then(res => {
-        let t0 = process.hrtime();
         res.data.map(e =>
             ({id: e.id, albumId: e.albumId, title: e.title, url: e.url, thumbnailUrl: e.thumbnailUrl}));
         let t1 = process.hrtime(t0);
@@ -20,8 +12,8 @@ function fetchTestPhotosToObj(url) {
 }
 
 function fetchTestPhotosToString(url) {
+    let t0 = process.hrtime();
     axios.get(url).then(res => {
-        let t0 = process.hrtime();
         JSON.stringify(res.data);
         let t1 = process.hrtime(t0);
         console.log(`Execution time: ${t1[0]}s ${t1[1] / 1000000}ms`);
